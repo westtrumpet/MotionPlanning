f2deadc13731cab3815ca4579f5fc0a545deee66
Alexander Westby <west.trumpet@gmail.com>
2015-10-21 23:10:07 -0500

    Creating repo for Unity Motion Planning

---
+++ Assets/MotionPlanner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionPlanner : MonoBehaviour {
+
+	public GameObject[] Walls;
+	public GameObject[] Obstacles;
+	public GameObject Agent;
+
+	void Start () {
+		
+	}
+}
